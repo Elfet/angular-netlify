d64f39598dd42f90900a8361e8c4b74f7af15313
Elffet <masahiro.sakai.ms@gmail.com>
2020-07-01 20:57:58 +0900

    変換できるようになった

--- src/app/input-area/input-area.component.ts
+++ src/app/input-area/input-area.component.ts
@@ -7,9 +7,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InputAreaComponent implements OnInit {
 
+  output: string = "";
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  conversion(text: string): void {
+    const texts = text.split("\n");
+    let times = new Map<number, string>();
+    let timecount = [];
+    for (let i = 0; i < texts.length; i++) {
+      if (/@[1][0-9]|[0-9]/g.test(texts[i])) {
+        let result = texts[i].match(/[1][0-9]|[0-9]/g);
+        let temp = result[0] + ":00";
+        times.set(i, temp);
+        timecount.push(i);
+      }
+      else if (/・/.test(texts[i])) {
+        texts[i] = "　" + texts[i];
+      } else {
+        texts[i] = "　　" + texts[i];
+      }
+    }
+    console.log(times);
+    let count = 1;
+    times.forEach(function(value, key) {
+      console.log(key+ " " + value);
+      let betweenTime = "";
+      if (key == timecount[timecount.length-1]) {
+        betweenTime = value + " ～ 17:20";
+        times.set(key, betweenTime);
+        texts[key] = betweenTime;
+        count++;
+      }
+      else {
+        betweenTime = times.get(key) + " ～ " + times.get(timecount[count]);
+        times.set(key, betweenTime);
+        texts[key] = betweenTime;
+        count++;
+      }
+    });
+    this.output = texts.join('\n');
+  }
 }
